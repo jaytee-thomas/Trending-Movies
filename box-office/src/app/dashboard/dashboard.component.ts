f156daaba87680306d77836b1f284c94ba07798f
Jaytee-Thomas <jt.aws.cloud@protonmail.com>
2023-01-14 20:25:21 -0600

    not done yet

--- box-office/src/app/dashboard/dashboard.component.ts
+++ box-office/src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Movie } from '../models/movie';
 import { MOVIES } from '../models/movies';
+
 
 @Component({
   selector: 'app-dashboard',
@@ -9,5 +10,26 @@ import { MOVIES } from '../models/movies';
 })
 export class DashboardComponent {
   title: string = 'Trending Movies';
-  movies: Movie[] = MOVIES;
+  movies = MOVIES;
+  searchText: string = '';
+  constructor() { }
+
+  ngOnInit(): void {  
+  }
+
+  search() {
+    if (this.searchText === '' || !this.searchText)
+    this.movies = MOVIES;
+  else {
+    this.movies = MOVIES;
+    this.movies = this.movies.filter(movie => movie.movieTitle?.startsWith(this.searchText.toLowerCase()));
+  }
+
+  }
+  reset() {
+    this.searchText = '';
+    this.movies = MOVIES;
+  }
+
+
 }
